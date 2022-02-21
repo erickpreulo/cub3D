7cd161ed3f37dfb6cbb52af92ee9b16b92b2acc5
Erick Gomes <erickgomes@Mac-mini-de-Erick.local>
2022-02-21 13:33:28 +0000

    validation map 2.0

--- src/init/validation.c
+++ src/init/validation.c
@@ -6,69 +6,74 @@
 /*   By: egomes <egomes@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 08:36:43 by aneuwald          #+#    #+#             */
-/*   Updated: 2022/02/21 12:55:11 by egomes           ###   ########.fr       */
+/*   Updated: 2022/02/21 13:32:48 by egomes           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
+void	validation_map_possible_characters(t_cub3d *cub3d, int i, int j)
+{
+	if (cub3d->config.map.map[i][j] != '0'
+		&& cub3d->config.map.map[i][j] != ' '
+		&& cub3d->config.map.map[i][j] != '1'
+		&& cub3d->config.map.map[i][j] != 'N'
+		&& cub3d->config.map.map[i][j] != 'S'
+		&& cub3d->config.map.map[i][j] != 'W'
+		&& cub3d->config.map.map[i][j] != 'E')
+		exit_error("MAP Character\n");
+}
+
+void	validation_map_shape(t_cub3d *cub3d, int i, int j)
+{
+	if ((j == 0) || (i == 0)
+		|| (j == cub3d->config.map.width - 1)
+		|| (i == cub3d->config.map.height - 1))
+	{
+		if (cub3d->config.map.map[i][j] != ' '
+			&& cub3d->config.map.map[i][j] != '1')
+			exit_error("MAP Shape\n");
+	}
+	else if (cub3d->config.map.map[i][j] == ' ')
+	{
+		if ((cub3d->config.map.map[i - 1][j] != ' '
+			&& cub3d->config.map.map[i - 1][j] != '1')
+			|| (cub3d->config.map.map[i][j - 1] != ' '
+			&& cub3d->config.map.map[i][j - 1] != '1')
+			|| (cub3d->config.map.map[i + 1][j] != ' '
+			&& cub3d->config.map.map[i + 1][j] != '1')
+			|| (cub3d->config.map.map[i][j + 1] != ' '
+			&& cub3d->config.map.map[i][j + 1] != '1'))
+			exit_error("MAP Shape\n");
+	}
+	validation_map_possible_characters(cub3d, i, j);
+}
+
 void	validation(void)
 {
-	t_cub3d *cub3d;
-	int i;
-	int j;
-	int player;
+	t_cub3d	*cub3d;
+	int		i;
+	int		j;
+	int		player;
 
 	player = 0;
 	cub3d = get_cub3d();
-	i = 0;
-	while (cub3d->config.map.map[i] != NULL)
+	i = -1;
+	while (cub3d->config.map.map[++i] != NULL)
 	{
-		j = 0;
-		while (cub3d->config.map.map[i][j] != '\0')
+		j = -1;
+		while (cub3d->config.map.map[i][++j] != '\0')
 		{
-			if ((j == 0) || (i == 0) || (j == cub3d->config.map.width - 1) || (i == cub3d->config.map.height - 1))
-			{
-				if (cub3d->config.map.map[i][j] != ' ' && cub3d->config.map.map[i][j] != '1')
-				{
-					printf("Error1: MAP\n[%s]\n", cub3d->config.map.map[i]);
-					exit_error("MAP\n");
-				}
-			}
-			else if (cub3d->config.map.map[i][j] == ' ')
-			{
-				if ((cub3d->config.map.map[i - 1][j] != ' ' && cub3d->config.map.map[i - 1][j] != '1')
-					|| (cub3d->config.map.map[i][j - 1] != ' ' && cub3d->config.map.map[i][j - 1] != '1')
-					|| (cub3d->config.map.map[i + 1][j] != ' ' && cub3d->config.map.map[i + 1][j] != '1')
-					|| (cub3d->config.map.map[i][j + 1] != ' ' && cub3d->config.map.map[i][j + 1] != '1'))
-				{
-					printf("Error2: MAP\n[%s]\n", cub3d->config.map.map[i]);
-					exit_error("MAP\n");
-				}
-			}
-			if (cub3d->config.map.map[i][j] != '0'
-				&& cub3d->config.map.map[i][j] != ' '
-				&& cub3d->config.map.map[i][j] != '1'
-				&& cub3d->config.map.map[i][j] != 'N'
-				&& cub3d->config.map.map[i][j] != 'S'
-				&& cub3d->config.map.map[i][j] != 'O'
-				&& cub3d->config.map.map[i][j] != 'L')
-			{
-				printf("Error3: MAP\n[%s]\n", cub3d->config.map.map[i]);
-				exit_error("MAP\n");
-			}
-			if (cub3d->config.map.map[i][j] == 'N' || cub3d->config.map.map[i][j] == 'S'
-				|| cub3d->config.map.map[i][j] == 'O' || cub3d->config.map.map[i][j] == 'L')
+			validation_map_shape(cub3d, i, j);
+			if (cub3d->config.map.map[i][j] == 'N'
+				|| cub3d->config.map.map[i][j] == 'S'
+				|| cub3d->config.map.map[i][j] == 'W'
+				|| cub3d->config.map.map[i][j] == 'E')
 			{
 				if (player)
-				{
-					printf("Error3: MAP\n[%s]\n", cub3d->config.map.map[i]);
-					exit_error("MAP\n");
-				}
+					exit_error("MAP Player\n");
 				player = 1;
 			}
-			j++;
 		}
-		i++;
 	}
 }
